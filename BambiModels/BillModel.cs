534867bb3958e35f4b8574b7fe954485650fc864
Josip Marijanović <jmarijano@foi.hr>
2018-09-19 21:33:12 +0200

    Added model classes for: BillModel, CustomerFeedbackModel, CustomerModel, DiscountModel, PaymentMethodModel, ReservationModel

---
+++ BambiModels/BillModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BambiModels
+{
+    public class BillModel
+    {
+        /// <summary>
+        /// The unique identifier for the bill.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The issue date of the bill.
+        /// </summary>
+        public DateTime DateOfIssue { get; set; }
+
+        /// <summary>
+        /// The reservation id for which the bill is.
+        /// </summary>
+        public int ReservationId { get; set; }
+
+        /// <summary>
+        /// The discount id for which the bill is.
+        /// </summary>
+        public int DiscountId { get; set; }
+
+        /// <summary>
+        /// The employee id which issued the bill.
+        /// </summary>
+        public string EmployeeId { get; set; }
+    }
+}
---
+++ BambiModels/CustomerModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BambiModels
+{
+    public class CustomerModel
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string TownZipCode { get; set; }
+
+        public string Address { get; set; }
+
+        public int ProfessionId { get; set; }
+
+        public int EducationTypeId { get; set; }
+
+        public char Gender { get; set; }
+    }
+}
