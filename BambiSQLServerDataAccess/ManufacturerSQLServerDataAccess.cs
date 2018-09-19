387e521dc1725b73f394a0b6137952037ff859d8
Josip Marijanović <jmarijano@foi.hr>
2018-09-19 21:35:17 +0200

    Created SQL server data access project.

---
+++ BambiSQLServerDataAccess/ManufacturerSQLServerDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BambiDataAccess;
+using BambiModels;
+using Dapper;
+using BambiFactory;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BambiSQLServerDataAccess
+{
+    public class ManufacturerSQLServerDataAccess : IManufacturerRepository
+    {
+        private IDbConnection _connection;
+        
+        
+        public ManufacturerSQLServerDataAccess(IDatabase connection)
+        {
+            _connection = connection.CreateConnection();
+        }
+        public void Insert(ManufacturerModel manufacturer)
+        {
+            using (_connection)
+            {
+                _connection.Query<ManufacturerModel>("SELECT * FROM Manufacturer");
+            }
+        }
+    }
+}
