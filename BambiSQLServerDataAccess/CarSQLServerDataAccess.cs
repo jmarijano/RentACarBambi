47bbd7965d11ef116a703a1c3724071f6657e4e5
Josip Marijanović <jmarijano@foi.hr>
2018-09-20 22:12:32 +0200

    Added data access classes for: Car and EmployeeType.

---
+++ BambiSQLServerDataAccess/CarSQLServerDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using BambiDataAccess;
+using BambiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BambiSQLServerDataAccess
+{
+    public class CarSQLServerDataAccess : ICarRepository
+    {
+        public void Delete(CarModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CarModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(CarModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CarModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BambiSQLServerDataAccess/EmployeeTypeSQLServerDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using BambiDataAccess;
+using BambiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BambiSQLServerDataAccess
+{
+    public class EmployeeTypeSQLServerDataAccess : IEmployeeTypeRepository
+    {
+        public void Delete(EmployeeTypeModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EmployeeTypeModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(EmployeeTypeModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(EmployeeTypeModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
