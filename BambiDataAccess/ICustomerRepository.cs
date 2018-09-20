ce65dc87e62f08e07bdce5dc584b086e7659ddc2
Josip Marijanović <jmarijano@foi.hr>
2018-09-20 22:12:00 +0200

    Added missing interfaces.

---
+++ BambiDataAccess/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using BambiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BambiDataAccess
+{
+    public interface ICustomerRepository
+    {
+        void Insert(CustomerModel model);
+        void Update(CustomerModel model);
+        void Delete(CustomerModel model);
+        IEnumerable<CustomerModel> GetAll();
+    }
+}
---
+++ BambiDataAccess/IEmployeeTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using BambiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BambiDataAccess
+{
+    public interface IEmployeeTypeRepository
+    {
+        void Insert(EmployeeTypeModel model);
+        void Update(EmployeeTypeModel model);
+        void Delete(EmployeeTypeModel model);
+        IEnumerable<EmployeeTypeModel> GetAll();
+    }
+}
